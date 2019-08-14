bb684d71d0dfb421d6b82a2c942736256e96401b
unknown <Sala300@TUNDFPCGE10F350.FormacionSena.RED>
2019-08-14 14:40:29 -0500

    Initial commit

---
+++ ProyectosSennova/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProyectosSennova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            int puntaje = 0;
+            Proyecto miProyecto = new Proyecto();
+            Proyecto miProyecto2 = new Proyecto();
+            Proyecto miProyecto3 = new Proyecto();
+            //Proyecto miProyecto4 = new Proyecto("Realidad aumentada", 12540, "Sistemas", 10);
+            Proyecto miProyecto5 = new Proyecto("Talento humano", 5);
+            Proyecto miProyecto6 = new Proyecto();
+
+            //miProyecto3.IngresarProyecto();
+            //miProyecto3.ImprimirProyecto();
+            //miProyecto4.ImprimirProyecto();
+            miProyecto5.ImprimirProyecto();
+            miProyecto6.IngresarProyecto();
+            miProyecto6.ImprimirProyecto();
+
+            //miProyecto.Area = "Sistemas";
+            //miProyecto.Codigo = 12000;
+            //miProyecto.Duracion = 13;
+            //miProyecto.Nombre = "Desarrollo internet de las cosas IOT";
+            //miProyecto2.Area = "Redes";
+            //miProyecto2.Codigo = 13000;
+            //miProyecto2.Duracion = 14;
+            //miProyecto2.Nombre = "Desarrollo Arduino";
+            //Console.WriteLine($" Area: {miProyecto.Area}");
+            //Console.WriteLine($" Codigo: {miProyecto.Codigo}");
+            //Console.WriteLine($" Duracion: {miProyecto.Duracion}");
+            //Console.WriteLine($" Nombre: {miProyecto.Nombre}");
+            //Console.WriteLine($" Area: {miProyecto2.Area}");
+            //Console.WriteLine($" Codigo: {miProyecto2.Codigo}");
+            //Console.WriteLine($" Duracion: {miProyecto2.Duracion}");
+            //Console.WriteLine($" Nombre: {miProyecto2.Nombre}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ProyectosSennova/Proyecto.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectosSennova
+{
+    public class Proyecto
+    {
+        #region Atributos
+
+        #endregion
+        #region Propiedades
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area { get; set; }
+        public int Duracion { get; set; }
+
+        public List<Proyecto> proyectos = new List<Proyecto>();
+        #endregion
+
+        #region Constructores
+        //ctor+tab+tab
+        public Proyecto(string nombre, double codigo, string area, int duracion)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+        }
+        public Proyecto()
+        {
+                
+        }
+        public Proyecto(string area, int duracion)
+        {
+            Area = area;
+            Duracion = duracion;
+        }
+
+        #endregion
+
+        #region Métodos
+        public void IngresarProyecto()
+        {
+
+            var res = "y";
+            while (res == "y")
+            {
+                Proyecto myProyecto = new Proyecto();
+
+                Console.WriteLine("Ingrese el Nombre del proyecto");
+                myProyecto.Nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el código");
+                myProyecto.Codigo = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el Area del proyecto");
+                myProyecto.Area = Console.ReadLine();
+                Console.WriteLine("Ingrese la duración del proyecto");
+                var dura = Console.ReadLine();
+                myProyecto.Duracion = int.Parse(dura);
+
+                proyectos.Add(myProyecto);
+
+                Console.WriteLine("Desea ingresar otro proyecto?  y/n");
+
+                res = Console.ReadLine();
+            }
+           
+           
+        }
+        public void ImprimirProyecto()
+        {
+            foreach (var item in proyectos)
+            {
+                Console.WriteLine($"El nombre del proyecto es: {item.Nombre}");
+                Console.WriteLine($"El codigo del proyecto es: {item.Codigo}");
+                Console.WriteLine($"El área es: {item.Area}");
+                Console.WriteLine($"La duración es:{item.Duracion}");
+            }
+
+            
+
+        }
+
+
+        #endregion
+
+
+
+    }
+
+
+}
